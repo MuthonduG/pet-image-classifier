f5b05ddb795870fe81e85bedb8c05d33bec36ef7
MuthonduG <muthondugithinji@gmai.com>
2023-11-11 02:57:42 +0300

    classifier

--- classifier.py
+++ classifier.py
@@ -1,65 +1,69 @@
 import ast
 from PIL import Image
+import torch
 import torchvision.transforms as transforms
 import torchvision.models as models
-import torch
 
-resnet18 = models.resnet18(pretrained=True)
-alexnet = models.alexnet(pretrained=True)
-vgg16 = models.vgg16(pretrained=True)
+class DogClassifier:
+    def __init__(self, model_name, dog_classes_file):
+        self.model = self.load_pretrained_model(model_name)
+        self.imagenet_classes_dict = self.load_imagenet_classes()
+        self.dog_classes = self.read_dog_classes(dog_classes_file)
+        self.transform = self.create_transform()
 
-models = {'resnet': resnet18, 'alexnet': alexnet, 'vgg': vgg16}
+    def load_pretrained_model(self, model_name):
+        model = getattr(models, model_name)(pretrained=True)
+        model.eval()
+        return model
 
-# obtain ImageNet labels
-with open('imagenet1000_clsid_to_human.txt') as imagenet_classes_file:
-    imagenet_classes_dict = ast.literal_eval(imagenet_classes_file.read())
+    def load_imagenet_classes(self):
+        with open('imagenet1000_clsid_to_human.txt') as imagenet_classes_file:
+            return ast.literal_eval(imagenet_classes_file.read())
 
-def read_dog_classes(file_path):
-    with open(file_path, 'r') as file:
-        print([line.strip().lower() for line in file])
+    def read_dog_classes(self, file_path):
+        with open(file_path, 'r') as file:
+            return [line.strip().lower() for line in file]
 
-def classify_image(img_path, model_name, dog_classes_file):
-    # Load the image
-    img_pil = Image.open(img_path)
+    def create_transform(self):
+        return transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
 
-    # Define transforms
-    preprocess = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
+    def classify_image(self, img_path):
+        img_pil = Image.open(img_path)
+        img_tensor = self.transform(img_pil)
+        img_tensor.unsqueeze_(0)
 
-    # Preprocess the image
-    img_tensor = preprocess(img_pil)
+        with torch.no_grad():
+            output = self.model(img_tensor)
 
-    # Resize the tensor (add dimension for batch)
-    img_tensor.unsqueeze_(0)
+        pred_idx = output.data.numpy().argmax()
+        predicted_label = self.imagenet_classes_dict[pred_idx].lower()
 
-    # Apply model to input
-    model = models[model_name]
+        if any(dog_class in predicted_label for dog_class in self.dog_classes):
+            return "Dog"
+        else:
+            return "Not a Dog"
 
-    # Put model in evaluation mode
-    model = model.eval()
+class CustomDogClassifier(DogClassifier):
+    def __init__(self, model_name, dog_classes_file, additional_dog_classes=None):
+        super().__init__(model_name, dog_classes_file)
+        
+        if additional_dog_classes is not None:
+            self.dog_classes.extend(additional_dog_classes)
 
-    # Apply data to model
-    with torch.no_grad():  # Use torch.no_grad() to disable gradient computation during inference
-        output = model(img_tensor)
-
-    # Return index corresponding to predicted class
-    pred_idx = output.data.numpy().argmax()
-
-    # Check if the predicted class corresponds to a dog-related class
-    dog_classes = read_dog_classes(dog_classes_file)
-    predicted_label = imagenet_classes_dict[pred_idx].lower()
-
-    if any(dog_class in predicted_label for dog_class in dog_classes):
-        print("Dog")
-    else:
-        print("Not a Dog")
+    def classify_image_from_path(self, img_path):
+        result = self.classify_image(img_path)
+        print(result)
 
 # Example usage
-img_path = "path/to/your/image.jpg"
 model_name = 'resnet'  # You can change this to 'alexnet' or 'vgg'
-dog_classes_file = "dogs.txt"
-classify_image(img_path, model_name, dog_classes_file)
+dog_classes_file = "./dogs.txt"
+additional_dog_classes = ["your_additional_dog_class"]
+
+custom_dog_classifier = CustomDogClassifier(model_name, dog_classes_file, additional_dog_classes)
+img_path = "./assets/dogs/d1.jpg"
+custom_dog_classifier.classify_image_from_path(img_path)
